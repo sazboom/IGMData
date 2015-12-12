7989ab1d66ac8e461b47ea2acad05823ee2e3731
Aaron Worsham <aaronworsham@gmail.com>
2015-12-12 10:43:20 -0500

    guard

---
+++ Database/dnd5Basic/actors/guard.js
@@ -0,0 +1,23 @@
+var dnd5BasicActorGuard = {
+	attackPaths : ['igm-data/Database/dnd5basic/attacks/shortSword'], 
+	defensePaths : [],
+	toHitStack : [	
+		{
+			
+		}
+	],
+	toAvoidStack : [],
+	onHitStack : [],
+	proficiencyBonus : 0,
+	attributes: 	{
+		'str':10,
+		'dex':10,
+		'con':10,
+		'wis':10,
+		'int':10,
+		'cha':10
+	}
+
+}
+
+module.exports = dnd5BasicActorGuard
