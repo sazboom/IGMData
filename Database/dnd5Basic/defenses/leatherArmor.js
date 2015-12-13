72030ff9c9f9082798be42788b9a8f8eafd67cb4
Aaron Worsham <aaronworsham@gmail.com>
2015-12-13 10:04:50 -0500

    added defenses dir

---
+++ Database/dnd5Basic/defenses/leatherArmor.js
@@ -0,0 +1,16 @@
+var dnd5BasicDefenseLeatherArmor = {
+	shortName: "leather armor",
+	longName: "leather armor",
+	defenseType: 'standard',
+	toAvoidHitStack: [
+		{
+			mechanic : 'armor',
+			effect : 'physical',
+			datatype : 'static',
+			bonus: 2			
+		}
+	],
+	onHitStack: []
+}
+
+module.exports = dnd5BasicDefenseLeatherArmor
